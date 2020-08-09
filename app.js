02eedfd9b868344470b572f58d4402e2a8f6f7f8
noorbutt <noorbuttnoorbutt706@gmail.com>
2020-08-09 16:27:40 +0500

    Quiz-App

---
+++ app.js
@@ -0,0 +1,73 @@
+function checked (){
+ var score = 0;
+var right_answer_1 = document.getElementById('q1')
+var q1_answer_2 = document.getElementById('q2')
+var q1_answer_3 = document.getElementById('q3')
+var q1_answer_4 = document.getElementById('q4')
+if(right_answer_1.checked == true){
+    score ++
+    alert('Q1 answer right')
+}else{
+    alert('Q1 answer wrong') 
+}
+
+
+
+ 
+var right_answer_2 = document.getElementById('Q1')
+var q2_answer_2 = document.getElementById('Q2')
+var q3_answer_2 = document.getElementById('Q3')
+var q4_answer_2 = document.getElementById('Q4')
+if(right_answer_2.checked == true){
+    score ++
+    alert('Q2 answer right')
+}else{
+    alert('Q2 answer wrong') 
+}
+
+
+
+ 
+var right_answer_3 = document.getElementById('que4')
+var q5_answer_2 = document.getElementById('que2')
+var q6_answer_2 = document.getElementById('que3')
+var q7_answer_2 = document.getElementById('que1')
+if(right_answer_3.checked == true){
+    score ++
+    alert('Q3 answer right')
+}else{
+    alert('Q3 answer wrong') 
+}
+
+
+
+
+
+
+var right_answer_4 = document.getElementById('qu1')
+var q5_answer_2 = document.getElementById('qu2')
+var q6_answer_2 = document.getElementById('qu3')
+var q7_answer_2 = document.getElementById('qu4')
+if(right_answer_3.checked == true){
+    score ++
+    alert('Q4 answer right')
+}else{
+    alert('Q4 answer wrong') 
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
